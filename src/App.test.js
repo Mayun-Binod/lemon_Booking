715db458434129c2a1a864c311cc3ea93eb2b91f
Mayun-binod <shresthabinod334@gmail.com>
2024-08-05 11:05:30 +0545

    lemon booking added

---
+++ src/App.test.js
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import BookingForm from './components/BookingForm';
+import Header from './components/Header';
+
+test('Renders the Header heading', () => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+    const headingElement = screen.getByText("Reserve Table");
+    expect(headingElement).toBeInTheDocument();
+
+    const reserveButton = screen.getByRole("button");
+    fireEvent.click(reserveButton);
+
+    const headingElementNew = screen.getByText("Choose Date");
+    expect(headingElementNew).toBeInTheDocument();
+})
+
+test('Initialize/Update Times', () => {
+  render(<BrowserRouter><App /></BrowserRouter>);
+  const reserveButton = screen.getByRole("button");
+  fireEvent.click(reserveButton);
+
+  const testTime = []
+
+
+})
